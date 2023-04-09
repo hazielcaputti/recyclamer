2b6ba023e33bd62b7f80d0d6978219148e5e49db
hazielcaputti <haziel.caputti@alu.ing.unlp.edu.ar>
2023-04-09 18:06:54 -0300

    commit inicial

---
+++ build/vrx/vrx_gazebo/CMakeFiles/vrx_gazebo__xacro_auto_generate_to_devel_space_.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/example_course.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/gymkhana.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/perception_task.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/scan_dock_deliver.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/stationkeeping_task.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/sydneyregatta.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/wayfinding_task.world"
+  "/home/haziel/vrx_ws/devel/share/vrx_gazebo/worlds/wildlife_task.world"
+  "CMakeFiles/vrx_gazebo__xacro_auto_generate_to_devel_space_"
+  "worlds/example_course.world"
+  "worlds/gymkhana.world"
+  "worlds/perception_task.world"
+  "worlds/scan_dock_deliver.world"
+  "worlds/stationkeeping_task.world"
+  "worlds/sydneyregatta.world"
+  "worlds/wayfinding_task.world"
+  "worlds/wildlife_task.world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vrx_gazebo__xacro_auto_generate_to_devel_space_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/vrx/vrx_gazebo/catkin_generated/vrx_gazebo-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(vrx_gazebo_MESSAGE_FILES "/home/haziel/vrx_ws/src/vrx/vrx_gazebo/msg/Task.msg;/home/haziel/vrx_ws/src/vrx/vrx_gazebo/msg/Contact.msg")
+set(vrx_gazebo_SERVICE_FILES "/home/haziel/vrx_ws/src/vrx/vrx_gazebo/srv/ColorSequence.srv;/home/haziel/vrx_ws/src/vrx/vrx_gazebo/srv/BallShooter.srv")
